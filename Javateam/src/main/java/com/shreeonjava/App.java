8db10f94af760fd97a9fd0f2ec9a5c420063817b
Shreshat <shreeshathenkabailu@gmail.com>
2025-05-06 13:53:59 +0530

    azure example

---
+++ Javateam/src/main/java/com/shreeonjava/App.java
@@ -0,0 +1,13 @@
+package com.shreeonjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
